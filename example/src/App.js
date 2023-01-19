0e21d0076b36fa75caa6fff194169e11361cd9a2
4illian <killian.occelli@epitech.eu>
2023-01-19 18:26:37 +0100

    initial

---
+++ example/src/App.js
@@ -0,0 +1,79 @@
+import './App.css';
+import {useEffect, useState} from "react";
+import ExampleComponent from "./Example.component";
+
+function App() {
+
+    // Un state du nombre de fois que le component enfant est demandé.
+    const [numberOfComponentRequired, setNumberOfComponentRequired] = useState(0)
+
+    // State contenant les résultats des différents components enfants
+    const [results, setResults] = useState([])
+
+
+    // Hook prenant pour callback le state "numberOfComponentRequired". A chacune des modifications apportées à "numberOfComponentRequired",
+    // ce hook sera activé et génèrera le state "results" adapté pour stocker toute la data des components enfants.
+    useEffect(() => {
+        let handle = []
+
+        for (let i = 0; i < numberOfComponentRequired; i++)
+            handle[i] = {}
+
+        setResults(handle)
+    }, [numberOfComponentRequired]);
+
+
+    // Function permettant d'avoir la main sur la modification du nombre de components demandé et d'y ajouter une limite
+    // pour éviter la génération abusive.
+    const handleChangeWithRestriction = async (setter, value) => {
+        // Limite à adapter --v
+        if (value < 11)
+            setter(value)
+        else setter(10)
+    };
+
+
+    // Fonction call depuis le component enfant qui set la key du state "results" modifiée.
+    function FunctionSetResults(key, value) {
+        results[key] = value
+        console.log("Tableau results -v-")
+        console.log(results)
+    }
+
+    return (
+        <div className="App">
+            Nombre de fois demandé :
+            <input
+                type={"number"}
+                onChange={((e) => {
+                    handleChangeWithRestriction(setNumberOfComponentRequired, e.target.value)
+                })}
+            />
+
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+            }}>
+                {
+                    // Mapping pour afficher le nombre de components demandé avec en passage de props les éléments nécessaires
+                    // à faire parvenir à l'enfant pour setter le state "results"
+                    Object.keys(results).map(function (item, i) {
+                        return (
+                            <ExampleComponent
+                                resultKey={i}
+                                functionSetResults={FunctionSetResults}
+                            />
+                        )
+                    })
+                }
+
+            </div>
+
+            <h2>La console contient un log de l'objet généré qui réapparait à chacune des modifications des components
+                enfants.</h2>
+        </div>
+    );
+}
+
+export default App;
---
+++ example/src/Example.component.js
@@ -0,0 +1,86 @@
+import React, {useEffect, useState} from 'react';
+
+const ExampleComponent = (props) => {
+
+        // Ici props contient donc :
+        // props.resultKey => key du tableau "results" nécessaire pour setter la bonne valeur
+
+        // ----
+
+        // States avec les différentes valeurs à stocker
+        const [value1, setValue1] = useState("")
+        const [value2, setValue2] = useState(0)
+        // Possibilité également de mettre tous les states (value1, value2....) dans un seul state sous format:
+        //
+        // state: {
+        //      value1: {},
+        //      value2: {},
+        //      ...
+        // }
+        //
+        // Dans ce cas penser à changer le callback du useEffect qui prend plus bas [value1, value2]
+        // ---
+
+
+        // Function pour setter des states (permet de les retrouver au même endroit).
+        const handleChange = async (setter, value) => {
+            setter(value)
+        };
+
+
+        // Hook appelé à la génération de la page car aucune restriction n'est imposé aux fields value1 et value2.
+        // Ce hook captera chaque modification de field et appelera la fonction du App.js "FunctionSetResults"
+        // pour set la key correspondante à l'appel de component dans le state "results".
+        useEffect(() => {
+            let formatedData = {
+                nomDeLaValeur1: value1,
+                nomDeLaValeur2: value2
+            }
+
+            // Call de FunctionSetResults pour setter le props "results" de la page App.js (main)
+            props.functionSetResults(props.resultKey, formatedData)
+
+            // Liste des callbacks --v
+        }, [value1, value2]);
+
+
+        return (
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                backgroundColor: "lightgray",
+                width: "25vw",
+                margin: 30,
+                padding: 10
+            }}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                }}>
+                    <p>valeur 1 du composant :</p>
+                    <input
+                        type={"text"}
+                        onChange={((e) => {
+                            handleChange(setValue1, e.target.value)
+                        })}
+                    />
+                </div>
+
+                <div style={{
+                    display: "flex",
+                    flexDirection: "row",
+                }}>
+                    <p>valeur 2 du composant :</p>
+                    <input
+                        type={"number"}
+                        onChange={((e) => {
+                            handleChange(setValue2, e.target.value)
+                        })}
+                    />
+                </div>
+            </div>
+        );
+    }
+;
+
+export default ExampleComponent;
